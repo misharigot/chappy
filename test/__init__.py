e8be94530bdaf78793c9e4eb6a57da61337a48b1
Heleen <heleeneisen98+git@gmail.com>
2020-12-01 20:02:06 +0100

    Fix "punctuator.punc not found" issue
    Packages were not only in src folder but also in another directory

--- test/__init__.py
+++ test/__init__.py
@@ -1,4 +1,6 @@
 import sys
 import os
+import sysconfig
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.venv/lib/python3.8/site-packages/")))
